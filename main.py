6076573c57f3bf9b257da86345987888e8a60088
notnotype <=>
2021-12-30 13:00:05 +0800

    fix #3 modify README.md

--- main.py
+++ main.py
@@ -25,12 +25,15 @@ def get_uuid():
 class XXT:
     course_id: str
     class_id: str
+    cpi: str
 
-    def __init__(self, course_id=None, class_id=None):
+    def __init__(self, course_id=None, class_id=None, cpi=None):
         if course_id:
             self.course_id = course_id
         if class_id:
             self.class_id = class_id
+        if cpi:
+            self.cpi = cpi
         self.default_headers = {
             'Host': 'exm-mayuan-ans.chaoxing.com',
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36'
@@ -67,7 +70,7 @@ class XXT:
 
     def get_exam_id(self, dir_id):
         url = 'https://exm-mayuan-ans.chaoxing.com/selftest/start-test' \
-              f'?courseId={self.course_id}&classId={self.class_id}&cpi=154457568&dirid={dir_id}&reset=true&protocol_v=1'
+              f'?courseId={self.course_id}&classId={self.class_id}&cpi={self.cpi}&dirid={dir_id}&reset=true&protocol_v=1'
         resp = self.get(url)
 
         # logger.debug('session headers: {!s}', self.client.headers.__str__())
@@ -100,6 +103,7 @@ class XXT:
         return question_answers
 
     def get_courses(self):
+        # new api https://mobile3.chaoxing.com/newmobile/getUserApps?fid=137737&isMergerMks=true&uid=109255777
         fid = self.client.cookies.get('fid', None)
         if not fid:
             raise RuntimeError('cookies中找不到fid， 未登录')
@@ -113,7 +117,7 @@ class XXT:
 
     def get_test_ids(self):
         url = f'https://exm-mayuan-ans.chaoxing.com/selftest/chapter-test' \
-              f'?courseId={self.course_id}&classId={self.class_id}&cpi=154457568'
+              f'?courseId={self.course_id}&classId={self.class_id}&cpi={self.cpi}'
         resp = self.get(url)
         resp.raise_for_status()
         html = HTML(resp.text)
@@ -132,10 +136,10 @@ class XXT:
 if __name__ == '__main__':
     # _dir_ids = [68773748]
 
-    xxt = XXT('203859054')
+    xxt = XXT('215780579', '2546', '98652693')
     xxt.set_cookie(
-        'k8s=94b55a395f76dcca7b5d74ef4f49561348c0490b; route=d4fc925c8d78ce1315b0eab056f65586; source=""; cookiecheck=true; AID_dsr=55; superlib=""; msign_dsr=1624672976934; search_uuid=9e9b93bd%2d0fde%2d4478%2dbdc2%2df9986ea58bfe; mqs=19e3b526c24d63963d23bb7a0b9fd5063452ab54e90deae287793d7e4e0031dca682df1732c2ee4183fccc602da3be02be4e8fb00cc31963da0c23a6adf5ac273ff78a53bee9da7f25b21bb28f6da16c81dc0ab09bfeb6109f7e6cd7c96a142b23ab17e1b3566a10f4c7c6fc68c7adcf; rt=-2; thirdRegist=1; s=11401F839C536D9E; _industry=5; web_im_hxToken=YWMtfdhJMtZ5EeuNRrUi9FEYVaKlE4D%2d6RHksYgJxSsb8jJZFa%5fwAkMR67k%2defI%2dKewcAwMAAAF6SEeI%5fABPGgAPIftrPDHSkMUmo4PkQspYvKvFPJMbC2qprcTG3HxI3A; web_im_tid=106990444; web_im_pic=http%3a%2f%2fphoto%2echaoxing%2ecom%2fphoto%5f80%2ejpg; web_im_name=%u848b%u4fca%u6770; KI4SO_SERVER_EC=RERFSWdRQWdsckNwckxwM2pXeFFSVGJRM3E5bytLQ0NNY0U1SHBIZ250bEtGbDVZSjJybStkSXd3%0ANkprU2VsMkRtU2djWlRjU2NTVQp1NG13bWtscWxXYWJhTEMyQnlnSnFKanZDVEREdzVzN3dlclJt%0ARDFtdWE0OVd4bXBHSTZockJmNDRYdlVvNVZVdmpjVGZ2STd3VEh1Ck5EcWZVcTVkWFY1eHJCSGJM%0ARXc4NnFVTnBsbFR1T0dhMzl1cDR3V0NMYUpXOFk5bERvRG9ZODA4d002UEFoQkw1NWZMR1hYd3Rm%0AL3EKRXNpYXFmR0wrQzFVYXZZaytuamcwOGNodEJGR3IzZytOak1OQjlWRWxidWRBcXhLMHVkVkxF%0AUzNib2JiNDhLeU1RM0QweDNIc0hYSgpsYjVjbUhFeDFydEJQQUlWclZTUHJRUzh6dk1DVHBib3FS%0AU2g3WWRoOW54c2xkRndvZzNxZVhKVEFUWkZpSlRSSEhucE5nSk9sdWlwCkZLSHRVNWlsM2ExQlR5%0AaWRFVnk5dkxDYXorTCtWWUtXVG5DMU1lNDBPcDlTcmwyckxocXM2WDluVVdNTUIvTnNnZVNUP2Fw%0AcElkPTEma2V5SWQ9MQ%3D%3D; _tid=106990444; sso_puid=152804638; duxiu=userName%5fdsr%2c%3dtfm%2c%21userid%5fdsr%2c%3d23224%2c%21char%5fdsr%2c%3d%u9551%2c%21metaType%2c%3d257%2c%21dsr%5ffrom%2c%3d1%2c%21logo%5fdsr%2c%3dlogo0408%2ejpg%2c%21logosmall%5fdsr%2c%3dsmall0408%2ejpg%2c%21title%5fdsr%2c%3d%u6e56%u5357%u79d1%u6280%u5927%u5b66%2c%21url%5fdsr%2c%3d%2c%21compcode%5fdsr%2c%3d1088%2c%21province%5fdsr%2c%3d%u6e56%u5357%2c%21readDom%2c%3d%2c%21isdomain%2c%3d3%2c%21showcol%2c%3d0%2c%21hu%2c%3d0%2c%21areaid%2c%3d0%2c%21uscol%2c%3d0%2c%21isfirst%2c%3d0%2c%21istest%2c%3d0%2c%21cdb%2c%3d0%2c%21og%2c%3d0%2c%21ogvalue%2c%3d0%2c%21testornot%2c%3d1%2c%21remind%2c%3d0%2c%21datecount%2c%3d3110%2c%21userIPType%2c%3d1%2c%21my%2c%3d1%2c%21lt%2c%3d0%2c%21ttt%2c%3dfxlogin%2echaoxing%2c%21enc%5fdsr%2c%3d2AD0B126EF91D739BEBE3F62A228A1C5; spaceFid=137737; spaceRoleId=""; tl=0; jrose=44765E6CF7812D5718977CBF30450261.self-exam-system-3910617459-881q7; uname=2004060213; lv=2; fid=1821; _uid=152804638; uf=b2d2c93beefa90dcd1ee03ad65e10855002362bacce7629380f5a15178273ea3b986de932964aa04ca6bfd70c21bcc03913b662843f1f4ad6d92e371d7fdf6446ece1d9f47db742bce71fc6e59483dd37d50523ec80a66bd8d3ee8bf68b09c216f7c04307e5fa421; _d=1624776714030; UID=152804638; vc=A36411C8D18DADE1DECFFC4CD2769FF4; vc2=130C99133255AF11FB6653EF592C8DDC; vc3=N2aGp9IxQZoVUtkTvshADjyrsoSYNgCPrLrNhX%2B6w8TfyagvqAwFODhs8PDBRnOcIVnl%2Fv4jfuPlEQZtxnW%2B0ieYf3UIHFSg8k6XiKcTLV021S9fVu7Am2vxGDbejH%2BRRLRFcRIMWRZhbctqeVvBWUAfiAFtEjrn0FffvC27N5Y%3D7d9b262d0da7ea90c9923fdf72931cea; xxtenc=20fa5b1fdc858614416da69204450107; DSSTASH_LOG=C_38-UN_55-US_152804638-T_1624776714032; EXAM_FID=1821')
-    xxt.get_class_id()
+        '_tid=76653558; sso_puid=109255777; _industry=5; fidsCount=3; k8s=bd0734ff408fe052a7df7557586b72e1a4822de8; route=36ef5ecbfa418bb6fd1c82c50e9f0066; schoolId=28948; examinationRole=8930d67295f8174b1469b2760b06a181; lv=2; fid=28948; _uid=109255777; UID=109255777; vc=43D57B2F0862A3BE50B0A7D5A7EF36F7; xxtenc=7cec5c59032021221fac4a42fb2f685b; jrose=BC228CCB7C7BD719F2E4822DF066CECB.self-exam-system-3661515132-4k7t2; uf=da0883eb5260151ed8722d802b2f755939c33fa9270a6a53cb316d38e6bde71a10c1943b3809c41e04f68592bfcd5b39dc436c455fddffc288b83130e7eb47045be6cb53ca92c59efd68be96b6183b1a7a5853460b99e4b1e71de711cfced6a4cbb59410137500e5; _d=1640838192883; vc2=0E6DD14E13562548AAB5DF0F0DEAA16C; vc3=a3BPmh3MNOPr5WnuovoymGQY9Zbgt6CqPMAIY56krplZfarL%2BXScJGdp6hctnsZw8sIRLqDE7s6O616LinbUUT6KuGvAkiCJBLdnzQjEIziQSGwAxYlV6q0TvMWbhW0L%2BnyXEnOZ%2FfkfGytW47%2FWsfOy8wpUrj6%2Bz7y0MM2NKmo%3Da3b8e0b9d91027fdae83fd3c3583867c; DSSTASH_LOG=C_38-UN_55-US_109255777-T_1640838192885; KI4SO_SERVER_EC=RERFSWdRQWdsckNwckxwM2pXeFFSYm56cm9PN0FsM0pxNlJDVHAwY0VEVEFlcGQ0ZzI0MDJ1MHd4%0AQUZDOXpKeERtU2djWlRjU2NTVQp1NG13bWtscWxXdHBSbWxzZ0RXMEsvcy9TR0FMVHlBN3dlclJt%0ARDFtdWE0OVd4bXBHSTZobzIwWGJJR3JZd3B1cGJ0Zmdqd0ZqRWxPCjhKbkRyZHlUMEZ6WTZTRkxz%0AYmo4cjhzaFI5NncrdUdhMzl1cDR3V0NMYUpXOFk5bERvRG9ZODA4d002UEFoQkw1NWZMR1hYd3Rm%0AL3EKRXNpYXFmR0wrQzFVYXZZaytuamcwOGNodEJGR3IzZytOak1OQjlWRWxidWRBcXhLMHVkVkxF%0AUzNib2JiNDhLeU1RM0QweDFZRjkxMwpLTGRvZVpzQm9Kbis2Y3p2RVlEaU5ndnM0aHJqd3JJeERj%0AUFRIWXo3bi9oazJ0cm4zMmx1ZDJMSXkyK0pCRUFOSExFbndPUENzakVOCnc5TWRXQmZkZHlpM2FI%0AbjJGNzVlK2RHZW1ub3dVUVVJYWRoZXJJdVJhVUp6bjVycU5SYzlCa3U2blFuMDh2VEgzbHJuP2Fw%0AcElkPTEma2V5SWQ9MQ%3D%3D; EXAM_FID=137737')
+    # xxt.get_class_id()
     _dir_ids = xxt.get_test_ids()
 
     r = xxt.get_courses()
@@ -163,9 +167,9 @@ if __name__ == '__main__':
         # answer = answer.replace('正确答案：', '')
         answer = answer.replace('正确答案：', '正确答案: ')
         answer = answer.replace('我的答案：', '')
-        # print(question, answer)
+        print(question, answer)
         text += f'{question}\n{answer}\n\n'
-    # print(text)
+    print(text)
 
     with open('out/' + datetime.datetime.now().strftime('%Y%m%d%H%M%S') + '.text', 'w+', encoding='utf8') as f:
         f.write(text)
